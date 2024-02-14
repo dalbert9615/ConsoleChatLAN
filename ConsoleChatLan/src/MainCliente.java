cbad034f8cc2213a51add5b7a3ea1bc3eebe2cf1
Shoce-Ubuntu <shoce.aguero@gmail.com>
2024-02-14 01:08:53 +0100

    Sockets (Cliente-Servidor)

---
+++ ConsoleChatLan/src/MainCliente.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+
+//Clase principal que hará uso del cliente
+public class MainCliente
+{
+    public static void main(String[] args) throws IOException
+    {
+        Cliente cli = new Cliente(); //Se crea el cliente
+
+        System.out.println("Iniciando cliente\n");
+        cli.startClient(); //Se inicia el cliente
+    }
+}
